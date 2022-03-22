10d7b8248cc4c298e2a7f327e1d323141f371720
Emmanuel Hernandez <86961961+EmmaxPlay@users.noreply.github.com>
2022-03-22 15:51:02 -0600

    tipo de gastos

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -20,6 +20,37 @@ const Modal = ({setModal, animarModal}) => {
             <legend>
                 Nuevo Gasto
             </legend>
+            <div className="campo">
+                <label htmlFor="nombre">Nombre gasto</label>
+                <input 
+                type="text" 
+                id="nombre"
+                placeholder="Ingresa el nombre del gasto" />
+            </div>
+            <div className="campo">
+                <label htmlFor="cantidad">Cantidad</label>
+                <input 
+                type="text" 
+                id="cantidad"
+                placeholder="Ingresa la cantidad del gasto" />
+            </div>
+            <div className="campo">
+                <label htmlFor="categoria">Categoria</label>
+                <select
+                id="categoria">
+                    <option value="">-- Seleccione --</option>
+                    <option value="ahorro">Ahorro</option>
+                    <option value="comida">Comida</option>
+                    <option value="casa">Casa</option>
+                    <option value="ocio">Ocio</option>
+                    <option value="general">En general</option>
+                    <option value="ahorro">Ahorro</option>
+                    <option value="suscripciones">Suscripciones</option>
+                </select>
+            </div>
+            <input 
+            type="submit"
+            value="Añadir gasto"/>
         </form>
     </div>
   )
